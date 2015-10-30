0f5c6720000886ee2f1773d777153e65d932edb2
ALS <sinyawskiy@gmail.com>
2015-10-30 10:14:52 +0300

    Update models.py

--- models.py
+++ models.py
@@ -17,6 +17,9 @@ class FavoriteManager(models.Manager):
     def create_favorite(self, user, obj):
         """create Favorite for obj and user.
         """
+        if not isinstance(obj, models.Model):
+            raise ValueError('value must be a Model object')
+
         content_type = ContentType.objects.get_for_model(type(obj))
         favorite = Favorite(user=user, content_type=content_type,
                             object_id=obj.pk, content_object=obj)
@@ -26,18 +29,27 @@ class FavoriteManager(models.Manager):
     def del_favorite(self, user, obj):
         """del user's Favorite for obj.
         """
+        if not isinstance(obj, models.Model):
+            raise ValueError('value must be a Model object')
+
         content_type = ContentType.objects.get_for_model(type(obj))
         self.get_queryset().filter(user=user, content_type=content_type, object_id=obj.pk).delete()
 
     def favorites_obj_of_user(self, user, model_class):
         """get user's obj. It can tell whether user has favorite obj
         """
+        if not isinstance(model_class, models.base.ModelBase):
+            raise ValueError('value must be a Model class')
+
         content_type = ContentType.objects.get_for_model(model_class)
         return self.get_queryset().filter(user=user, content_type=content_type)
 
     def favorites_for_obj(self, model_class):
         """get favorites for specific object.
         """
+        if not isinstance(model_class, models.base.ModelBase):
+            raise ValueError('value must be a Model class')
+
         content_type = ContentType.objects.get_for_model(model_class)
         return self.get_queryset().filter(content_type=content_type)
 
