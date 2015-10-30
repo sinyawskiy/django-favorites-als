f0cc6b86ef69112cee8eec342c32f985635ab5ba
ALS <sinyawskiy@gmail.com>
2015-10-30 10:14:21 +0300

    Update favorite_tags.py

--- templatetags/favorite_tags.py
+++ templatetags/favorite_tags.py
@@ -2,19 +2,23 @@
 from favorites.models import Favorite
 from django.db import models
 from django import template
+
+
 register = template.Library()
 
 
+@register.filter
 def favorite_count(value):
-    if not isinstance(value, models.Model):
-        raise ValueError('value must be a Model object')
+    if not isinstance(value, models.base.ModelBase):
+        raise ValueError('value must be a Model class')
     count = Favorite.objects.favorites_for_obj(value).count()
     return str(count)
 
-register.filter('favorite_count', favorite_count)
 
-
+@register.filter
 def is_in_favorites(user, favorite_object):
-    return favorite_object in [favorite.content_object for favorite in Favorite.objects.favorites_obj_of_user(user, type(favorite_object))]
-
-register.filter('is_in_favorites', is_in_favorites)
+    if not isinstance(favorite_object, models.Model):
+        raise ValueError('favorite_object must be a Model object')
+    return favorite_object in [
+        favorite.content_object for favorite in Favorite.objects.favorites_obj_of_user(user, type(favorite_object))
+    ] if user.is_authenticated() else False
